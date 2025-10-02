7da0536697957cf63d7eb71ae1be4e558fb08972
Salman <salmanhammed77@gmail.com>
2025-10-03 00:15:25 +0530

    fix: cleanup telescope

---
+++ src/entrypoints/telescope-ui/Telescope.stories.svelte
@@ -0,0 +1,24 @@
+<script module>
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import Telescope from "./Telescope.svelte";
+  import { fn } from "storybook/test";
+
+  // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+  const { Story } = defineMeta({
+    title: "Telescope/main",
+    component: Telescope,
+    tags: ["autodocs"],
+    argTypes: {
+      backgroundColor: { control: "color" },
+      size: {
+        control: { type: "select" },
+        options: ["small", "medium", "large"],
+      },
+    },
+    args: {
+      onclick: fn(),
+    },
+  });
+</script>
+
+<Story name="Primary" args={{ primary: true, label: "Button" }} />
